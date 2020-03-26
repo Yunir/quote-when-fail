576e7b4c5553c82db4c849d5b1164eebc486082a
Yunir <yunir.me@gmail.com>
2020-03-26 15:29:49 +0300

    handle situation when the website is unreachable

--- src/main/kotlin/com/yunir/QuoteGrabber.kt
+++ src/main/kotlin/com/yunir/QuoteGrabber.kt
@@ -5,9 +5,13 @@ import org.jsoup.nodes.Element
 
 class QuoteGrabber {
     fun grabNewQuote() {
-        val doc = Jsoup.connect("http://bash.org/?random").get()
-        val link: Element = doc.select("p.qt").first()
-        link.select("br").append("\\n")
-        println(link.text().replace("\\n ", "\n"))
+        try{
+            val doc = Jsoup.connect("http://bash.org/?random").get()
+            val link: Element = doc.select("p.qt").first()
+            link.select("br").append("\\n")
+            println(link.text().replace("\\n ", "\n"))
+        } catch (npe : IllegalStateException) {
+            println("Resource with quotes is unreachable")
+        }
     }
 }
